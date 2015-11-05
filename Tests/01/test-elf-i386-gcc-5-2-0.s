9d7066092918bab38e59fb2ae6918dc27ddc348f
Pavel Dovgalyuk <Pavel.Dovgaluk@gmail.com>
2015-11-05 13:17:40 +0000

    Test with buffer overflow in main

---
+++ Tests/01/test-elf-i386-gcc-5-2-0.s
@@ -0,0 +1,40 @@
+	.file	"test.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
+	subl	$20, %esp
+	subl	$12, %esp
+	leal	-12(%ebp), %eax
+	pushl	%eax
+	call	gets
+	addl	$16, %esp
+	subl	$12, %esp
+	leal	-12(%ebp), %eax
+	pushl	%eax
+	call	printf
+	addl	$16, %esp
+	movl	$0, %eax
+	movl	-4(%ebp), %ecx
+	.cfi_def_cfa 1, 0
+	leave
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 5.2.0"
+	.section	.note.GNU-stack,"",@progbits
---
+++ Tests/01/test.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+  char buf[4];
+  gets(buf);
+  printf(buf);
+  return 0;
+}
+
