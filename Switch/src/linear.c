79267e65638a3aff0b89ca4ae03ca55b7187d8ab
Dovgaluk <pavel.dovgaluk@gmail.com>
2015-12-13 16:37:01 +0300

    Added initial switch test

---
+++ Switch/src/linear.c
@@ -0,0 +1,15 @@
+int func(int a)
+{
+  switch (a)
+  {
+  case 1: return a;
+  case 2: return a+1;
+  case 3: return a*2;
+  case 4: return a+3;
+  case 5: return a-4;
+  case 6: return a+1;
+  case 7: return a-5;
+  case 8: return a+7;
+  }
+  return -1;
+}
