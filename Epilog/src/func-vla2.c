d9a958cc3eeb16b5e194ab325a60c897b27aa5b2
Pavel Dovgalyuk <pavel.dovgaluk@ispras.ru>
2016-09-01 12:34:37 +0300

    Added vla2 test

---
+++ Epilog/src/func-vla2.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+
+/*
+  Attributes: FUNC VLA
+*/
+
+void func(void)
+{
+  int sz, i;
+  scanf("%d", &sz);
+  for (i = 0 ; i < sz ; ++i)
+  {
+    char buf[sz];
+    gets(buf);
+    printf(buf);
+  }
+}
+
+int main()
+{
+  func();
+  return 0;
+}
